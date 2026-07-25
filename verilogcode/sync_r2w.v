2506fb01b4e72be4ce06d5cc7337f8c9888d9f74
Vineetha <139565767+vineetha-kammarachedu@users.noreply.github.com>
2026-07-25 13:12:00 +0530

    Update and rename sync_r2w.v to verilogcode/sync_r2w.v

--- verilogcode/sync_r2w.v
+++ verilogcode/sync_r2w.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/19/2026 03:52:28 PM
-// Design Name: 
-// Module Name: sync_r2w
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module sync_r2w #(
