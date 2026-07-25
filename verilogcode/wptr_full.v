97a0ad3d26297e7985b7c923f8c5bfb71043bc53
Vineetha <139565767+vineetha-kammarachedu@users.noreply.github.com>
2026-07-25 13:12:47 +0530

    Update and rename wptr_full.v to verilogcode/wptr_full.v

--- verilogcode/wptr_full.v
+++ verilogcode/wptr_full.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/19/2026 03:41:51 PM
-// Design Name: 
-// Module Name: wptr_full
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module wptr_full #(
