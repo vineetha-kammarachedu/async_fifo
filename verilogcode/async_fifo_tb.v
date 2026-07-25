ded77434267cf5aab30b4f2cd9582886b8a6a3a6
Vineetha <139565767+vineetha-kammarachedu@users.noreply.github.com>
2026-07-25 13:08:24 +0530

    Update and rename async_fifo_tb.v to verilogcode/async_fifo_tb.v

--- verilogcode/async_fifo_tb.v
+++ verilogcode/async_fifo_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/19/2026 04:04:28 PM
-// Design Name: 
-// Module Name: async_fifo_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module async_fifo_tb;
